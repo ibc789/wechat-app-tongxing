ea8eb6077368385d46c36dd208d26e921d4382fd
Howard Yeh <howard@metacircus.com>
2016-09-23 17:30:38 +0800

    can toggle star

--- page/profile.js
+++ page/profile.js
@@ -1,25 +1,25 @@
+let idCounter = 2;
 Page({
     data: {
         newMessageText: "",
         messages: [
             {
+                id: 1,
                 message: 'TypeScript 用起来的确不错',
                 createdAt: "2016/09/21",
                 stars: 2,
+                isLiked: false,
             },
 
             {
+                id: 2,
                 message: 'main = putStrLn "hello world"',
                 createdAt: "2016/09/21",
                 stars: 4,
-            }
+                isLiked: true,
+            },
         ],
     },
-
-    // doesn't do anything
-    // bindKeyInput(e) {
-    //     console.log("key input", e);
-    // },
 
     bindchange(e) {
         const newMessageText = e.detail.value;
@@ -27,16 +27,21 @@ Page({
         const { messages } = this.data;
 
         const now = new Date();
+        idCounter++;
+
         messages.unshift({
+            id: idCounter,
             message: newMessageText,
+            stars: 0,
+            isLiked: false,
             createdAt: `${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`,
         });
 
         // TODO use immutable
+
         this.setData({
             newMessageText: "",
             messages,
-            stars: 0,
         });
     },
 
@@ -53,6 +58,24 @@ Page({
 
     bindblur(e) {
         console.log("blur");
+    },
+
+    toggleStar(e) {
+        const id = parseInt(e.target.dataset.id);
+
+        let messages = this.data.messages;
+        const message = this.data.messages.find(message => Object.is(id, message.id));
+
+        // FIXME: use immutable structure
+        message.isLiked = !message.isLiked;
+        if (message.isLiked) {
+            message.stars += 1;
+        } else {
+            message.stars -= 1;
+        }
+
+        // trigger update...
+        this.setData({messages});
     },
 
 
